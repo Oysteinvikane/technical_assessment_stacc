b5eeca2410a04e7c2159868c002bde2fe57ab79e
Øystein Helmersen Vikane <hvikane94@gmail.com>
2023-02-20 04:32:57 +0100

    minor changes

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import App from "./App";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <App />
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="flex-grow bg-gray-200">
+      <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-gray-900">Projects</h1>
+        <p className="mt-2 text-gray-600">This is the projects page!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
