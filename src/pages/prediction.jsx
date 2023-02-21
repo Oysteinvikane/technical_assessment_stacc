940b202b7e48f728eb18c83207490237fb778b30
Øystein Helmersen Vikane <hvikane94@gmail.com>
2023-02-21 21:04:06 +0100

    test prediction

--- src/pages/prediction.jsx
+++ src/pages/prediction.jsx
@@ -13,7 +13,7 @@ function Prediction() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch("https://flask-api-navy.vercel.app/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ input: Object.values(inputs).map(parseFloat) }),
