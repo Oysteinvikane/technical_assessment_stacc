416ebf574d0cf0517ccb174322976cfebb6cb917
Øystein Helmersen Vikane <hvikane94@gmail.com>
2023-02-22 04:54:35 +0100

    Couldn't sleep, so did some finishing touches

--- src/pages/prediction.jsx
+++ src/pages/prediction.jsx
@@ -17,6 +17,9 @@ function Prediction() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch("http://127.0.0.1:5000/predict", {
+      // const response = await fetch(
+      //   "https://flask-api-oysteinvikane.vercel.app/predict",
+      //   {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ input: Object.values(inputs).map(parseFloat) }),
